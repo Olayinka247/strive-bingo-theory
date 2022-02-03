83e0e3450fa32c351bdb832518d0aa66fa516eaa
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-02-03 16:28:33 +0000

    Bingo Board solution

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-alert("MY BINGO")
+const onLoad = function() {
+    createCells()
+}
+window.onload = onLoad 
+
+// Creating the cell numbers
+
+const createCells = function (){
+
+    let numberOfCells = 76
+
+    let cellContainerNode = document.getElementById('cells-container')   
+
+    for (let cellNumbers = 1; cellNumbers <= numberOfCells; cellNumbers++){
+
+// creating a new div for the cell numbers
+    let cellDivNode = document.createElement('div')
+// About to customize and add a class
+
+    cellDivNode.innerText = cellNumbers
+    cellDivNode.classList.add("cell")
+
+// attaching it to the DOM
+    cellContainerNode.appendChild(cellDivNode)
+}
+    
+
+
+}
+
+const bingoRandomNumbers = () => {
+    let myRandomNumber = []
+    for(let i=0; i<24; i++) {
+        let randomNum = Math.floor(Math.random() * 77) 
+        myRandomNumber.push(randomNum)
+        let randomNumberNode = document.createElement('div')
+        randomNumberNode.classList.add('randomNumber')
+        randomNumberNode.innerText = myRandomNumber[i]
+        document.getElementById('randomNumber').appendChild(randomNumberNode)
+    }
+
+    console.log(myRandomNumber)
+}
